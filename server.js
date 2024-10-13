cd6565e78d180a83cd5d19423a925ebf03532341
Ahmad_Nehela <60977296+Nehelaaa@users.noreply.github.com>
2024-10-12 22:12:53 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv').config();  // Load environment variables from .env
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-// Connect to MongoDB using the environment variable for MongoDB URI
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log('Connected to MongoDB Atlas'))
 .catch((error) => console.error('MongoDB connection error:', error));
 
